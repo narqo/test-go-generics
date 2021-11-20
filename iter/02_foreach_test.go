93a88f17ef38a7e465d3f2b7332d385b3118691e
Vladimir Varankin <vladimir@varank.in>
2021-11-21 00:13:56 +0100

    iter: add foreach example

---
+++ iter/02_foreach_test.go
@@ -0,0 +1,39 @@
+package iter_test
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/narqo/test-go-generics/iter"
+)
+
+func ExampleForEach() {
+	a := &sliceIterator[int]{
+		[]int{1, 2, 3},
+	}
+
+	forEach[int](
+		a,
+		func(v int) {
+			fmt.Println(v)
+		},
+	)
+
+	// Output:
+	// 1
+	// 2
+	// 3
+}
+
+func forEach[T any](it iter.Iterator[T], f func(T)) error {
+	for {
+		v, err := it.Next()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		f(v)
+	}
+}
